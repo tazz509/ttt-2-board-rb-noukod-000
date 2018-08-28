653950b9f491a656c563e68e4e251483d681f949
carly <carly@noukod.com>
2018-08-28 17:59:47 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -2,28 +2,28 @@
 row = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 # Position 1: top Left
-row[0] = "1"
+row[0]  
 
 # Position 2: top Middle
-row[1] = "2"
+row[1] 
 
 # Position 3: top Right
-row[2] = "3"
+row[2]
 
 # Position 4: Left
-row[3] = "4"
+row[3]
 
 # Position 5: Middle
-row[4] = "5"
+row[4]
 
 # Position 6: Right
-row[5] = "6"
+row[5]
 
 # Position 7: bottom Left
-row[6] = "7"
+row[6]
 
 # Position 8: bottom Middle
-row[7] = "8"
+row[7]
 
 # Position 9: bottom Right
-row[8] = "9"
+row[8]
