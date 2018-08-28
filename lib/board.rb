2f02b79d272198b42fd92b5ee7803ef3e9566ecc
carly <carly@noukod.com>
2018-08-28 18:10:16 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,29 @@
 # Define the variable board below.
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+# Position 1: top Left
+board[0]
+
+# Position 2: top Middle
+board[1]
+
+# Position 3: top Right
+board[2]
+
+# Position 4: Left
+board[3]
+
+# Position 5: Middle
+board[4]
+
+# Position 6: Right
+board[5]
+
+# Position 7: bottom Left
+board[6]
+
+# Position 8: bottom Middle
+board[7]
+
+# Position 9: bottom Right
+board[8]
