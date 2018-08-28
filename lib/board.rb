ee5f69f79448541f0caa99a1b0cb509c47772253
carly <carly@noukod.com>
2018-08-28 17:44:00 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,33 @@
 # Define the variable board below.
+row1= [" ", " ", " "]
+
+# Position 1: Left
+row[0]
+
+# Position 2: Middle
+row[1]
+
+# Position 3: Right
+row[2]
+
+row2= [" ", " ", " "]
+
+# Position 4: Left
+row[0]
+
+# Position 5: Middle
+row[1]
+
+# Position 6: Right
+row[2]
+
+row3= [" ", " ", " "]
+
+# Position 7: Left
+row[0]
+
+# Position 8: Middle
+row[1]
+
+# Position 9: Right
+row[2]
